e9d81d915ac86a78d6d83d9d0160a459f1ed2852
Jimmy <jimmy.chen@ceosoftcenters.com>
2016-02-17 16:14:45 +0800

    Initial Commit

---
+++ ScreenShotAndDraw/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScreenShotAndDraw
+//
+//  Created by Jimmy on 16/2/17.
+//  Copyright © 2016年 Jimmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScreenShotAndDraw/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScreenShotAndDraw
+//
+//  Created by Jimmy on 16/2/17.
+//  Copyright © 2016年 Jimmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
